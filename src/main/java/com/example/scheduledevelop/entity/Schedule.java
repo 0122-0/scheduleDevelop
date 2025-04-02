b4f15e49c43b7891785028375aee452a9df677d2
0122-0 <flowercats95@gmail.com>
2025-04-02 14:29:51 +0900

    잘못된 테이블 정보 수정

--- src/main/java/com/example/scheduledevelop/entity/Schedule.java
+++ src/main/java/com/example/scheduledevelop/entity/Schedule.java
@@ -10,13 +10,14 @@ public class Schedule extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 임의로 아이디값을 받음.
     private Long id; // 서버에서 적용되는 임의 번호 아이디
 
-    @Column(nullable = false) // null은 안되지만 동명이인으로 인한 unique 설정은 하지않음
-    private String name; // 유저이름
-
+    @Column(nullable = false) //null을 하게되면 의미가 없어지는것이니 안됨.
     private String todotitle; // 할일 제목
 
+    @Column(columnDefinition = "longtext")
     private String todo; // 할일
 
     //BaseEntity 상속 받아서 작성일과 수정일 존재
-
+    @ManyToOne //연관관계
+    @JoinColumn(name = "user_id") // 유저의 id로 유저 고유 식별자 연관관계설정
+    private User user; // 유저 고유 식별자
 }
--- src/main/java/com/example/scheduledevelop/entity/User.java
+++ src/main/java/com/example/scheduledevelop/entity/User.java
@@ -10,6 +10,9 @@ public class User extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 임의로 아이디 값을 받음
     private Long id; // 서버에서 적용되는 임의 번호 아이디
 
+    @Column(nullable = false) // null은 안되지만 동명이인으로 인한 unique 설정은 하지않음
+    private String name; // 유저이름
+
     @Column(nullable = false, unique = true) // null 불가, 같은 이메일 사용 방지를 위한 unique설정
     private String email; // 유저 이메일
 
